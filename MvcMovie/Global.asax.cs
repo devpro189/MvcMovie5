84a4c426b77165eceb86e0bccd399208f0dc5f69
科学楼 <cts-wzhao@live.com>
2015-06-03 11:36:16 +0800

    dummy change
    dummy change

--- MvcMovie/Global.asax.cs
+++ MvcMovie/Global.asax.cs
@@ -12,7 +12,6 @@ namespace MvcMovie
     {
         protected void Application_Start()
         {
-            //something first
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
